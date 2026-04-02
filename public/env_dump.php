c339bb4555d36c15720902af61608de8fd166ccb
Tiagotj7 <tiagowork07@gmail.com>
2026-04-01 21:24:52 -0300

    Remove unused test files: db_test.php, env_dump.php, env_test.php, and path_test.php

--- public/env_dump.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../bootstrap/config.php';
-var_dump(DB_HOST, DB_NAME, DB_USER);
-echo "<pre>";
-var_dump($GLOBALS['APP_ENV_VARS'] ?? null);
-echo "</pre>";
--- public/path_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$expected = realpath(__DIR__ . '/../env.ini');
-echo "Trying: " . __DIR__ . "/../env.ini<br>";
-echo "realpath: " . ($expected ?: 'NOT FOUND') . "<br>";
-
-echo "file_exists: " . (file_exists(__DIR__ . '/../env.ini') ? 'YES' : 'NO') . "<br>";
-echo "is_readable: " . (is_readable(__DIR__ . '/../env.ini') ? 'YES' : 'NO') . "<br>";
